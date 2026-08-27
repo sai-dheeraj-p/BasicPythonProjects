ff09259f066474df8be5ff5b5af18aebe069368f
Sai Dheeraj Perumbudur <psaidheeraj36@gmail.com>
2026-08-27 11:41:26 +0530

    innitial commit

---
+++ operators.py
@@ -0,0 +1,32 @@
+a3='-'*70
+#addn operators
+a = 2
+b = 3
+print('a is 2 & b is 3')
+print('a+b =',a+b)
+print(a3)
+#subs operators
+a = 2
+b = 3
+print('a is 2 & b is 3')
+print('a-b =',a-b)
+print(a3)
+#multiplication operators
+a = 2
+b = 3
+print('a is 2 & b is 3')
+print('a*b =',a*b)
+print(a3)
+#division operators
+print('5/5 =',5 / 5)
+print('10/2 =',10 / 2)
+print('-10/2 =',-10 / 2)
+print('20.0/2',20.0 / 2)
+print('5//3=',5//3)
+print('20//3 =',20 // 3)
+print('6%2=',6%2)
+print(a3)
+#exponent op
+a = 2
+b = 3
+print('a*b =',a**b)
---
+++ prnt_inpt.py
@@ -0,0 +1,12 @@
+a1="-"*80
+print('hellow world')
+print("hello","drj")
+print("abc is not equal to 123 ",end=":)")
+print(" :0 :/")
+print(1,2,3,4,5,sep="__")
+print(a1)
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("welcome "+name)
+print("you're age is : "+age)
